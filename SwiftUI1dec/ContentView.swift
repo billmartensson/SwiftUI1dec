37077bd3a25e04293392dd254498e0a6481207a2
Bill Mårtensson <bill@magictechnology.se>
2020-12-01 09:49:02 +0100

    Initial Commit

---
+++ SwiftUI1dec/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI1dec
+//
+//  Created by Bill Martensson on 2020-12-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI1dec/SwiftUI1decApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI1decApp.swift
+//  SwiftUI1dec
+//
+//  Created by Bill Martensson on 2020-12-01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI1decApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
